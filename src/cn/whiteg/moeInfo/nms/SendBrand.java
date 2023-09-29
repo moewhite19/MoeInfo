fca565d49fd67e058df876e556418213dded286e
MoeWhite19 <MoeWhite19@163.com>
2023-09-29 21:48:03 +0800

    适配更新到版本号1.20.2

--- src/cn/whiteg/moeInfo/nms/SendBrand.java
+++ src/cn/whiteg/moeInfo/nms/SendBrand.java
@@ -3,10 +3,12 @@ package cn.whiteg.moeInfo.nms;
 import cn.whiteg.moeInfo.utils.EntityNetUtils;
 import io.netty.buffer.Unpooled;
 import net.minecraft.network.PacketDataSerializer;
-import net.minecraft.network.protocol.game.PacketPlayOutCustomPayload;
+import net.minecraft.network.protocol.common.ClientboundCustomPayloadPacket;
+import net.minecraft.network.protocol.common.custom.BrandPayload;
 import org.bukkit.entity.Player;
 
 public class SendBrand {
+
     public static void send(Player player,String brand) {
         //brand = PAPIHook.getPAPIString(player, brand);
         //Validate.notNull(player,"Player is null!");
@@ -14,6 +16,6 @@ public class SendBrand {
         if (player == null) return;
         if (brand == null) return;
         var np = EntityNetUtils.getNmsPlayer(player);
-        EntityNetUtils.getPlayerConnection(np).a(new PacketPlayOutCustomPayload(PacketPlayOutCustomPayload.a,new PacketDataSerializer(Unpooled.buffer()).a(brand)));
+        EntityNetUtils.getPlayerConnection(np).a(new ClientboundCustomPayloadPacket(new BrandPayload(new PacketDataSerializer(Unpooled.buffer()).a(brand))));
     }
 }
